e714786c43eeaae738c80ed9a8c60653a1e131ba
Nick <dynarithm@gmail.com>
2021-03-05 17:37:18 +0200

    Add required AI code & update env.sample

---
+++ ai/DiscourseGraph.py
@@ -0,0 +1,62 @@
+import sys
+import ai.config as config
+
+from py2neo import Graph
+from neo4j import ServiceUnavailable
+
+from ai.neo4j_wrapper import Neo4jDatabase
+from ai.graph_algos import GraphAlgos
+from ai.delete import delete_discourse_graph
+from ai.utils import load_models, counter
+from ai.select import summarize_communities
+from ai.create import (
+    create_node_groups_edges,
+    create_discourse_graph,
+    create_similarity_graph
+)
+
+
+def main():
+    # Load all required models.
+    en_nlp, el_nlp, lang_det = load_models()
+
+    # Connect to the database.
+    try:
+        database = Neo4jDatabase(config.uri, config.username, config.password)
+        graph = Graph(config.uri, auth = (config.username, config.password))
+    except ServiceUnavailable as error: # The Neo4j server is unavailable.
+        print('\t* Neo4j database is unavailable.')
+        print('\t* Please check the database connection before running this script.')
+        sys.exit(1)
+
+    # Create node groups and edges from the json file.
+    node_groups, edges = create_node_groups_edges(
+        config.input_path, config.node_types, config.fields)
+
+    # Delete the entire discourse graph.
+    exists = delete_discourse_graph(database, node_groups)
+    if not exists: return # If the discource is empty, return early.
+
+    # Create the discourse graph in the Neo4j Database.
+    create_discourse_graph(graph, node_groups, edges, config.fields)
+    
+    # Create the similarity graph.
+    create_similarity_graph(graph, node_groups, edges, 
+                            config.node_types, config.fields, 
+                            en_nlp, el_nlp, lang_det, config.cutoff)
+
+    # Calculate the community score for the similarity graph.
+    with GraphAlgos(database, ['Node'], ['is_similar']) as graph:
+        graph.louvain(write_property = 'community')
+
+    # Summarization debug.
+    for id, item in summarize_communities(database, en_nlp, el_nlp, lang_det, 
+                                          config.top_n, config.top_sent).items():
+        print(f'{id}: {item[0]}\n')
+        print(f'{id}: {item[1]}\n')
+        print(f'{id}: {item[2]}\n')
+        print(f'{id}: {item[3]}\n')
+        print()
+
+
+if __name__ == '__main__': main()
